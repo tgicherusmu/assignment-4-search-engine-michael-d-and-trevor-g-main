01719aeb2f66d7fbf9f31589ceb1b056a02e0d75
tgicherusmu <112418001+tgicherusmu@users.noreply.github.com>
2023-04-02 12:49:36 -0500

    Update Tree.h

---
+++ Tree.h
@@ -0,0 +1,9 @@
+//a tree called Node represents a node in the lubrary search tree
+//the class has a pointer to the root node of the binary search tree and an integer representing its class
+//the function height will take a pointer to a node and its argument will to return its height and the pointer null will return -1 
+//the function max will take two integers as its arguments and return the maximum of them
+//the function insert takes an element of type T as its argument and iserts it into a binary seach tree. if the element already exists it will return reference to it otherwise it will create a new node with this element and insert it into the appropreiate pistion in the tree
+//the function balanace take sapointer to a node as its argument and balaces of the subtree rooted at this ode using only one of 4 rotation operations
+ //the functions rotateWithLeftChild rotateWithRightChild doubleWithLeftChild and doubleWithRightChild are private helper function tha tperform one of four rotataion operations on a subtree rooted at a given node
+
+ 
