c378b45300a330125847202599998c57559c1c71
tgicherusmu <112418001+tgicherusmu@users.noreply.github.com>
2023-04-02 12:42:27 -0500

    Update DocumentParser.cpp

---
+++ DocumentParser.cpp
@@ -0,0 +1,10 @@
+//changeFile need to take in a string as an argument and set FileName variable to that string
+//parseDoc needs to read in a file using ifsreamm and wraps it using StreamWrapper
+//getText returns the text from the JSON file
+//getText need to return text from JSON file
+//getTextBlurb needs to return a string that contains the first 32 words of text from the JSON file
+//getTitle needs to return the title of teh file
+//getPublishDate needs to return the published date of JSON
+//getSite needs to return the URL of the file
+//getPerson needs to return a list of people mentioned in the file
+//getOrgs needs to return a list of organizations mentioned in the file
