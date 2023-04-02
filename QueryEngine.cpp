525194e02c60c677de4571162aff1cc81a921719
tgicherusmu <112418001+tgicherusmu@users.noreply.github.com>
2023-04-02 12:48:44 -0500

    Update QueryEngine.cpp

---
+++ QueryEngine.cpp
@@ -0,0 +1,2 @@
+//changeWord takes a string called word as its argument and sets the class variable Word to this value, then calls the function stemWord
+//function stemWord ges the stemWord of the word stoed in the class varibale word it first trims this word by removing any non alphabetic charccter and converts all alphabetsic character sto lowercase using the porter libarry
