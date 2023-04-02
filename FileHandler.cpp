307af7cbddce2c55247b69efa831d3a87339a9fd
tgicherusmu <112418001+tgicherusmu@users.noreply.github.com>
2023-04-02 12:43:21 -0500

    Update FileHandler.cpp

---
+++ FileHandler.cpp
@@ -0,0 +1,8 @@
+//updateIndex parses in string file
+//queryTreeWord takes in string words, integers count, and integer type
+//queryTree will then update the word and get documents for the word then checks if this i the first word being checked or not
+//If it is not the first word then it checks if the set is empty or not
+//If its empty it becomes an AND search
+//If it is an OR search that it puts all items into the main set
+//if it is the first word being searched then it assigns a set to intersect
+
