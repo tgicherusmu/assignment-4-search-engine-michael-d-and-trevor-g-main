e5db24118fe43b9d5f7059e2f7260284176f17b0
tgicherusmu <112418001+tgicherusmu@users.noreply.github.com>
2023-04-02 12:47:22 -0500

    Update IndexEntry.cpp

---
+++ IndexEntry.cpp
@@ -0,0 +1,7 @@
+//addDocToldxEntry will take a string called docName as its argument and add it to the index entry
+//second method called operator== takes an IndexEntry called s as its argument returns true if indexentry's word field is less than s's word field
+//operator< takes an IndexEntry called s as its argument and returns  true true if this InexEntry's Word field is less than s's word field
+//operator> takes an IndexEntry called s as its argument and returns true if this IndexEntry's word field is greater than s's word field
+//docNameSeInsert takes a string called word as an argument and puts it in DocNamesSet
+//docNamesMapInsert takes a string called word and an integer called freq as its arguments and inserts them in to DocNames
+
