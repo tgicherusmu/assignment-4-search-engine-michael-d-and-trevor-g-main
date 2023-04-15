7917b0b20d8109165b665c1fe42f035126093660
Trevor Gicheru <112418001+tgicherusmu@users.noreply.github.com>
2023-04-15 16:41:17 -0500

    Update IndexEntry.cpp

--- IndexEntry.cpp
+++ IndexEntry.cpp
@@ -13,7 +13,7 @@ void IndexEntry::addDocToIdxEntry(const string& docName){
     auto it= DocNames.find(docName);
     if(it!=DocNames.end()){
         it->second++;
-        //return;
+        return;
     }
     // or add doc
     else{
