69c2fc7685e139be23a624cd99d1c7616ba7ac0e
Trevor Gicheru <112418001+tgicherusmu@users.noreply.github.com>
2023-04-25 14:40:47 -0500

    Update Document.h

--- Document.h
+++ Document.h
@@ -11,7 +11,14 @@
 
 using namespace std;
 class Document {
-   private:
+    //CODE REVIEW COMMENTS:
+    //return all functions by reference
+    //make all variables public
+    //vector of people, vector of orgs instead of string
+    //Delete rapidjson document, instead when making document object pass json document by reference and automatically fill all the strings  in .h file (doing this in constructor)
+   public:
+    // TA Adam E: I added a default constructor for Document, because
+    Document() = default;
     string FileName;
     string text;
     string textBlurb;
@@ -22,13 +29,11 @@ class Document {
     string person;
     string orgs;
     rapidjson::Document d;  // TA Adam E: I added the data-member Document d
-
-   public:
-    // TA Adam E: I added a default constructor for Document, because
-    Document() = default;
     Document(string f) { FileName = f; };
     void changeFile(string);
+    //^^^^^^delete this and create new document object instead
     void parseDoc();
+    //^^^^^^this can be deleted
     string getText();
     string getTextBlurb();
     string getTitle();
@@ -37,6 +42,7 @@ class Document {
     string getURL();
     list<string> getPerson();
     list<string> getOrgs();
+    //^^^^all get functions can be deleted as well,ex. replace every getPerson with .person
     int maxChars = 1000000;
 };
 
