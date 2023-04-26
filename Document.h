8ee388b80fbe3278a78fe959f661c6f56d5d3a4c
Trevor Gicheru <112418001+tgicherusmu@users.noreply.github.com>
2023-04-26 17:50:28 -0500

    Update Document.h

--- Document.h
+++ Document.h
@@ -5,6 +5,11 @@
 #include <fstream>
 #include <iostream>
 #include <list>  //TA Adam E: This is the include statement for linked lists
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
 
 #include "rapidjson/document.h"
 #include "rapidjson/istreamwrapper.h"
@@ -16,38 +21,62 @@ class Document {
     //make all variables public
     //vector of people, vector of orgs instead of string
     //Delete rapidjson document, instead when making document object pass json document by reference and automatically fill all the strings  in .h file (doing this in constructor)
-   public:
+  public:
     // TA Adam E: I added a default constructor for Document, because
-    Document() = default;
     string FileName;
     string text;
-    string textBlurb;
+    string uuid;
     string title;
     string publishDate;
     string site;
     string url;
-    string person;
-    string orgs;
-    rapidjson::Document d;  // TA Adam E: I added the data-member Document d
-    Document(string f) { FileName = f; };
-    void changeFile(string);
+    vector<string> people;
+    vector<string> orgs;
+//    rapidjson::Document d;  // TA Adam E: I added the data-member Document d
+    explicit Document(const string& filepath) { //todo: test this constructor
+        FileName = filepath;
+        ifstream f(filepath);
+        rapidjson::IStreamWrapper wrap(f);
+        rapidjson::Document d;
+        d.ParseStream(wrap);
+
+        uuid = d["uuid"].GetString();
+        site = d["thread"]["site"].GetString();
+        url = d["url"].GetString();
+        title = d["title"].GetString();
+        publishDate = d["published"].GetString();
+        text = d["text"].GetString();
+
+        for (auto& person : d["entities"]["persons"].GetArray()) {
+            string name = person["name"].GetString();
+            //todo: clean name, make sure it is all lowercase and replace spaces with '-'
+            people.emplace_back(name);
+        }
+
+        for (auto& organ : d["entities"]["organizations"].GetArray()) {
+            string name = organ["name"].GetString();
+            //todo: clean name, make sure it is all lowercase and replace spaces with '-'
+            orgs.emplace_back(name);
+        }
+    };
+//    void changeFile(string);
     //^^^^^^delete this and create new document object instead
-    void parseDoc();
-    vector<string> getPersons() const; 
-    vector<string> getOrgs() const; 
-    vector<string> persons_vec; 
-    vector<string> orgs_vec; 
+
+    //vector<string> getPersons() const;
+    //vector<string> getOrgs() const;
+    //vector<string> persons_vec;
+    //vector<string> orgs_vec;
     //^^^^^^this can be deleted
-    string getText();
-    string getTextBlurb();
-    string getTitle();
-    string getPublishDate();
-    string getSite();
-    string getURL();
-    list<string> getPerson();
-    list<string> getOrgs();
+//    string getText();
+//    string getTextBlurb();
+//    string getTitle();
+//    string getPublishDate();
+//    string getSite();
+//    string getURL();
+//    list<string> getPerson();
+//    list<string> getOrgs();
     //^^^^all get functions can be deleted as well,ex. replace every getPerson with .person
-    int maxChars = 1000000;
+//    int maxChars = 1000000;
 };
 
 #endif
