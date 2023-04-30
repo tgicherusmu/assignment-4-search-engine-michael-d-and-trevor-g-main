8e6b8d47d8e51cebe48540d8d202ead63270b059
DeWeese-Michael <76921430+DeWeese-Michael@users.noreply.github.com>
2023-04-30 00:55:56 -0500

    Update test_Tree.cpp

--- test_Tree.cpp
+++ test_Tree.cpp
@@ -5,27 +5,26 @@
 #include "Tree.h"
 #include "FilterEntry.h"
 #include "catch2/catch.hpp"
+// add includes
 
-TEST_CASE("Test AvlTree class with integers")
+TEST_CASE("Test AvlTree", "[AvlTree]")
 {
-    AvlTree<int> tree;
-    REQUIRE(tree.isEmpty() == true);
-    tree.insert(10);
-    REQUIRE(tree.isEmpty() == false);
-    tree.insert(20);
-    REQUIRE(tree.contains(20) == true);
-    tree.remove(20);
-    REQUIRE(tree.contains(20) == false);
+    AvlTree<int> t;
+    REQUIRE(t.isEmpty() == true);
+}
+TEST_CASE("Check that AVLNode and AVLTree::Tree works", "[AVLTree::Tree]") {
+// add test cases (create lists and specify what the expected output is)
 
-    // Test persistence
-    std::ofstream out("test.txt");
-    out << tree;
-    out.close();
+SECTION("Check that AVLTree::Tree works", "[AVLTree::Tree]"){
+        AvlTree<int> rhs;
+        REQUIRE(rhs.isEmpty() == true);
+        rhs.insert(1);
+        rhs.insert(2);
+        rhs.insert(3);
+        rhs.insert(4);
 
-    AvlTree<int> newTree;
-    std::ifstream in("test.txt");
-    in >> newTree;
-    in.close();
-
-    REQUIRE(newTree.contains(10) == true);
+        rhs.prettyPrintTree();
+        rhs.makeEmpty();
+        rhs.clearElements();
+        rhs.getEntries();
 }
